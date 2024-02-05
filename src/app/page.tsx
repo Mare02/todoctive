1f2d0dbc09ccbedc4c84b51cece76514887b8607
markoobradovic <marko123obradovic@gmail.com>
2024-02-05 22:26:26 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,8 @@ export default function homePage() {
   return (
     <div>
       <div className='text-center mb-10'>
-        <h1 className='text-4xl font-semibold'>Dashboard</h1>
+        <h1 className='text-4xl font-semibold'>Home Page</h1>
       </div>
-
-      <Dashboard />
     </div>
   )
 }
